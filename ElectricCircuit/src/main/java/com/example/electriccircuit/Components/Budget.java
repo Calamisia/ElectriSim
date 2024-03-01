4d3ff4d86570adbac53cd1f8bf66e0009580274a
Calamisia <wcielen68@gmail.com>
2024-03-01 10:06:19 -0500

    Made a budget class as well as set, get methods for all components.

--- ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
@@ -8,7 +8,24 @@ public class Budget {
     final double RESISTORBASE = 2.00;
     double budget;
 
-    public double createWirePrice(Wire wire)
+    public void setBudget(double budget){
+        this.budget = budget;
+    }
+    public double getBudget(){
+        return budget;
+    }
+    public double createWirePrice(Wire wire) {
+        return (wire.getLength() * WIREBASE);
+    }
+    public double createCapacitorPrice(Capacitors capacitor) {
+        return (capacitor.getCapacitance() * CAPACITORBASE);
+    }
+    public double createPowerSupplyPrice(PowerSupply powerSupply) {
+        return (powerSupply.getVoltage() * POWERSUPPLYBASE);
+    }
+    public double createResistorPrice(Resistors resistor) {
+        return (resistor.getResistance() * RESISTORBASE);
+    }
 
 
 }
--- ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Resistors.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Resistors.java
@@ -1,4 +1,14 @@
 package com.example.electriccircuit.Components;
 
 public class Resistors {
+
+    private int resistance;
+
+    public void setResistance(int resistance) {
+        this.resistance = resistance;
+    }
+
+    public int getResistance() {
+        return resistance;
+    }
 }
