7b2faf18319d18aabc8dce69a7e68d619a51f429
Calamisia <wcielen68@gmail.com>
2024-03-01 09:38:34 -0500

    Testing integrated Pushing

--- ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
@@ -3,11 +3,12 @@ package com.example.electriccircuit.Components;
 public class Budget {
 
     final double WIREBASE = 1.00;
-    //final double
-//hi
-    //hi
+    final double CAPACITORBASE = 5.00;
+    final double POWERSUPPLYBASE = 10.00;
+    final double RESISTORBASE = 2.00;
+    double budget;
 
-
+    public double createWirePrice(Wire wire)
 
 
 }
