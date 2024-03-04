4345a6dc70f55bbed8b148a6d5f92b36f4e25367
Calamisia <wcielen68@gmail.com>
2024-03-04 16:21:12 -0500

    Revert "Merge branch 'GUI-Testing2' into WorkingTesting"
    This reverts commit a1d466002c2b3dd2357811fa3a3da6c5e40a02bf, reversing
    changes made to 90680c1832bd124d2d3820150bf95f6e47728084.

--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
@@ -3,37 +3,20 @@ package com.example.electriccircuit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
-    private boolean isFullScreen = false;
-
     @Override
     public void start(Stage stage) throws IOException {
-        //Load the fxml file
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-
-        //Set up the scene
-        Scene scene = new Scene(fxmlLoader.load());
-
-        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
-            if (event.getCode() == KeyCode.F11) {
-                isFullScreen = !isFullScreen;
-                stage.setFullScreen(isFullScreen);
-            }
-        });
-
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
-    }
 
-    public static void main(String[] args) {
-        launch();
+
     }
 }
--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloController.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloController.java
@@ -1,37 +1,9 @@
 package com.example.electriccircuit;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloController {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    public void switchtoScene1(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    public void switchtoScene2(ActionEvent event) throws IOException {
-        root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
     @FXML
     private Label welcomeText;
 
