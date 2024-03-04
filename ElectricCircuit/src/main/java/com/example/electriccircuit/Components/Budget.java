9e3c3145d370d689df63d13d9bbcc8418ea00315
Calamisia <wcielen68@gmail.com>
2024-03-04 10:59:52 -0500

    Fixed budget class

--- ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/Components/Budget.java
@@ -2,30 +2,35 @@ package com.example.electriccircuit.Components;
 
 public class Budget {
 
+    // Price scalars for each component
     final double WIREBASE = 1.00;
     final double CAPACITORBASE = 5.00;
     final double POWERSUPPLYBASE = 10.00;
     final double RESISTORBASE = 2.00;
-    double budget;
+    double budget; // the budget will be assigned for each level
 
     public void setBudget(double budget){
         this.budget = budget;
-    }
+    } // set budget
     public double getBudget(){
         return budget;
-    }
+    } // get budget\
+
+    // determine the price of components
     public double createWirePrice(Wire wire) {
         return (wire.getLength() * WIREBASE);
     }
+
     public double createCapacitorPrice(Capacitors capacitor) {
         return (capacitor.getCapacitance() * CAPACITORBASE);
     }
+
     public double createPowerSupplyPrice(PowerSupply powerSupply) {
         return (powerSupply.getVoltage() * POWERSUPPLYBASE);
     }
+
     public double createResistorPrice(Resistors resistor) {
         return (resistor.getResistance() * RESISTORBASE);
     }
-
 
 }
