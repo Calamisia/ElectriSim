b3a6c5fb86dad934dd7ff49a81653db680b0266c
FizzyBooy <victorjipa4@gmail.com>
2024-03-04 13:58:03 -0500

    Finished rough drafts of screens

--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
@@ -3,24 +3,37 @@ package com.example.electriccircuit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private boolean isFullScreen = false;
+
     @Override
     public void start(Stage stage) throws IOException {
-      /*  Rectangle rectangle = new Rectangle();
-        GridPane grid = new GridPane();
-        SplitPane splitpane = new SplitPane(grid);
-        Pane pane = new Pane(splitpane);
-        */
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scene1.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //Load the fxml file
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
+        //Set up the scene
+        Scene scene = new Scene(fxmlLoader.load());
+
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.F11) {
+                isFullScreen = !isFullScreen;
+                stage.setFullScreen(isFullScreen);
+            }
+        });
+
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+    }
 
-
+    public static void main(String[] args) {
+        launch();
     }
 }
