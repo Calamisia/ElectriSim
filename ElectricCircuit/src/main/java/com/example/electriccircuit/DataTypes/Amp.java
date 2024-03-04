5bc92ac0876078a8813673bad029f9774565487b
Calamisia <wcielen68@gmail.com>
2024-03-04 09:40:27 -0500

    Redid operator classes to follow UML

--- ElectricCircuit/src/main/java/com/example/electriccircuit/DataTypes/Amp.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/DataTypes/Amp.java
@@ -1,30 +1,19 @@
 package com.example.electriccircuit.DataTypes;
-
-
 import com.example.electriccircuit.Logic.Physics;
 
 public class Amp extends Physics {
     private double current;
-    /* constructor directly with the value given */
+
     public Amp(double current){
         this.current = current;
-    }
-    /* using ohms law to get the current from the volts and ohms */
-    public Amp(Volt potential, Ohm resistance) {
-        this.current = ohmsLaw(potential,resistance);
-    }
-    /* using the power law to get the current from the power and the potential */
-    public Amp(Watt power, Volt potential) {
-        this.current = powerLaw(potential,power);
-    }
+    } // constructer with value given
 
-    /* setters */
     public void setAmp(double current){
         this.current = current;
-    }
+    } //setter
 
-    /* getters */
     public double getAmp(){
         return current;
-    }
+    } //getter
+
 }
--- ElectricCircuit/src/main/java/com/example/electriccircuit/DataTypes/Volt.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/DataTypes/Volt.java
@@ -1,5 +1,4 @@
 package com.example.electriccircuit.DataTypes;
-
 import com.example.electriccircuit.Logic.Physics;
 
 public class Volt extends Physics {
@@ -7,25 +6,14 @@ public class Volt extends Physics {
 
     public Volt(double potential){
         this.potential = potential;
-    }
-
-    public Volt(Amp current, Ohm resistance){
-        this.potential = ohmsLaw(current, resistance);
-    }
-
-    public Volt(Watt power, Amp current){
-        this.potential = powerLaw(power, current);
-    }
-
-    public Volt(Charge coulomb, Capacitance farad){
-        this.potential = capacitanceLaw(coulomb, farad);
-    }
-
-    public double getVolt(){
-        return potential;
-    }
+    } // constructor
 
     public void setVolt(double potential){
         this.potential = potential;
-    }
+    } // setter
+
+    public double getVolt(){
+        return potential;
+    } // getter
+
 }
