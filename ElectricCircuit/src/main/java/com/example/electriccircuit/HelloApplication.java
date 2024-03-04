d50610d2e60990e1cf031d027e84159e1f330ad5
Calamisia <wcielen68@gmail.com>
2024-03-04 11:25:25 -0500

    .

--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
@@ -10,11 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-      /*  Rectangle rectangle = new Rectangle();
-        GridPane grid = new GridPane();
-        SplitPane splitpane = new SplitPane(grid);
-        Pane pane = new Pane(splitpane);
-        */
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
