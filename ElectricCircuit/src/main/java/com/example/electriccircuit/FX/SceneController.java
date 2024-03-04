2c676fbecee248152ba7a3dde3183c883f48c71d
Calamisia <wcielen68@gmail.com>
2024-03-04 10:44:59 -0500

    fix

---
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/FX/SceneController.java
@@ -0,0 +1,42 @@
+package com.example.electriccircuit.FX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchtoScene1(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene1.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchtoScene2(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("scene2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
