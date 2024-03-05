e3a1abaa050061b98d64e22e5d7f68ea4825c856
Calamisia <wcielen68@gmail.com>
2024-03-05 16:20:34 -0500

    Commented everything

--- ElectricCircuit/src/main/java/com/example/electriccircuit/Logic/Unlocks.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/Logic/Unlocks.java
@@ -3,24 +3,28 @@ package com.example.electriccircuit.Logic;
 public class Unlocks {
     private String achievementBitString;
 
+    // no-arg constructor
     public Unlocks(){
         achievementBitString = "0000000000";
     }
 
     public String getAchievementBitString() {
         return achievementBitString;
-    }
+    } // getter
 
     public void setAchievementBitString(String achievementBitString) {
         this.achievementBitString = achievementBitString;
-    }
+    } // setter
 
     public boolean isAchievementUnlocked(int achievementIndex){
         if (achievementBitString.charAt(achievementIndex) == 1)
             return true;
         else return false;
-    }
+    } // checks if a specific achievement is unlocked, used to display x's on achievement screen
 
+
+    // this is used to see if you have enough achievements to access a level.
+    // level {6,7,8,9} require {1,3,5,8} achievements respectively.
     public boolean isLevelUnlocked(int levelAsked){
         int sum = 0;
         for (int i = 0; i < 10; i++) {
