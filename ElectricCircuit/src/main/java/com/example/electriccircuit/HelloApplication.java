8c3ebaf0a8385af390f194ac6c781e74cd08fcd2
FizzyBooy <victorjipa4@gmail.com>
2024-03-21 20:21:40 -0400

    Really good progress on changing screens and changing the way things works

--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
@@ -2,28 +2,60 @@ package com.example.electriccircuit;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-import com.example.electriccircuit.Logic.BuilderMatrix;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    private Stage primaryStage;
+    private Scene scene;
+    private Node scene1,scene2,scene3,scene4;
+    private FXMLLoader fxmlLoader1,fxmlLoader2,fxmlLoader3,fxmlLoader4;
+    private HelloController controller1,controller2,controller3,controller4;
+
     @Override
     public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("title.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene = new Scene(fxmlLoader.load());
+
+        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("title.fxml"));
+        scene1 = fxmlLoader1.load();
+
+        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("LevelSelection.fxml"));
+        scene2 = fxmlLoader2.load();
+
+        FXMLLoader fxmlLoader3 = new FXMLLoader(HelloApplication.class.getResource("Achievements screen.fxml"));
+        scene3 = fxmlLoader3.load();
+        controller3 = fxmlLoader3.getController();
+
+        FXMLLoader fxmlLoader4 = new FXMLLoader(HelloApplication.class.getResource("MainScreen.fxml"));
+        scene4 = fxmlLoader4.load();
+
+        HelloController controller = fxmlLoader.getController();
+        controller.setMain(this);
+        controller.titleinitialize();
+
         primaryStage.setTitle("Hello!");
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
 
-        HelloController controller = fxmlLoader.getController();
-        controller.titleinitialize();
-
     }
+
+    public HelloController AchievementsController(){
+        return controller3;
+    }
+    public Node switchToAchievements(){
+        return scene3;
+    }
+
+    public Node switchToMainScreen(){
+        return scene4;
+    }
+
 }
