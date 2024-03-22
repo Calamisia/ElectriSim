f948e7c39674df2cdd7d8a53b983766183e88adc
OrangeMold <64652407+OrangeMold@users.noreply.github.com>
2024-03-22 09:31:41 -0400

    Revert "trying to fix stuff"
    This reverts commit dae04f43351ceb9f082888df0619c8b253f27e1e, reversing
    changes made to 7501f20442f3e8fcb286ca1cd655f0d0e6a0a001.

--- ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
+++ ElectricCircuit/src/main/java/com/example/electriccircuit/HelloApplication.java
@@ -4,9 +4,8 @@ import com.example.electriccircuit.Logic.CalculatingGrid;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import com.example.electriccircuit.Logic.BuilderMatrix;
@@ -16,83 +15,19 @@ import java.io.IOException;
 import static com.example.electriccircuit.Logic.SaveFiles.saveGame;
 
 public class HelloApplication extends Application {
-
-    private Stage primaryStage;
-    private Scene scene;
-    private Node scene1,scene2,scene3,scene4;
-    private FXMLLoader fxmlLoader1,fxmlLoader2,fxmlLoader3,fxmlLoader4;
-    private HelloController controller1,controller2,controller3,controller4;
-
-    // Add mainContainer
-    private StackPane mainContainer = new StackPane();
-
     @Override
     public void start(Stage primaryStage) throws IOException {
-        this.primaryStage = primaryStage;
-
-        // Initialize mainContainer
-        mainContainer = new StackPane();
-
-        scene = new Scene(mainContainer);
-
-        FXMLLoader fxmlLoader1 = new FXMLLoader(HelloApplication.class.getResource("title.fxml"));
-        scene1 = fxmlLoader1.load();
-        controller1 = fxmlLoader1.getController();
-
-        FXMLLoader fxmlLoader2 = new FXMLLoader(HelloApplication.class.getResource("LevelSelection.fxml"));
-        scene2 = fxmlLoader2.load();
-        controller2 = fxmlLoader2.getController();
-
-        FXMLLoader fxmlLoader3 = new FXMLLoader(HelloApplication.class.getResource("Achievements screen.fxml"));
-        scene3 = fxmlLoader3.load();
-        controller3 = fxmlLoader3.getController();
-
-        FXMLLoader fxmlLoader4 = new FXMLLoader(HelloApplication.class.getResource("MainScreen.fxml"));
-        scene4 = fxmlLoader4.load();
-        controller4 = fxmlLoader4.getController();
-
-
-        //set the main for every screen
-        controller1.setMain(this);
-        controller2.setMain(this);
-        controller3.setMain(this);
-        controller4.setMain(this);
-
-        controller1.titleinitialize();
-
-        primaryStage.setOnCloseRequest( e -> {
-            saveGame();
-            CalculatingGrid calc = new CalculatingGrid();
-        });
-
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("title.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         primaryStage.setTitle("Hello!");
         primaryStage.setScene(scene);
         primaryStage.setMaximized(true);
         primaryStage.show();
-
+        HelloController controller = fxmlLoader.getController();
+        controller.titleinitialize();
+        primaryStage.setOnCloseRequest( e -> {
+            saveGame();
+            CalculatingGrid calc = new CalculatingGrid();
+        });
     }
-
-    public StackPane getMainContainer() {
-        return mainContainer;
-    }
-
-    //Title screen switching methods
-    public Node switchToTitle(){
-        return scene1;
-    }
-    public HelloController TitleController(){return controller1;}
-
-    //Achievements switching methods
-    public HelloController AchievementsController(){
-        return controller3;
-    }
-    public Node switchToAchievements(){
-        return scene3;
-    }
-
-    //MainScreen switching methods
-    public Node switchToMainScreen(){
-        return scene4;
-    }
-
 }
